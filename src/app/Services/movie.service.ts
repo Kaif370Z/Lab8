e1b170b72620ea3f8aff209d317514312e6473d6
Kaif <kaiftahir00@gmail.com>
2022-03-30 10:36:39 +0100

    lab8

---
+++ src/app/Services/movie.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+
+  constructor(private HttpClient:HttpClient) { }
+
+  GetMovieData():Observable<any>{
+    return this.HttpClient.get('http://www.omdbapi.com/?apikey=6462cf83&s=%27war%27');
+
+
+  }
+
+}
